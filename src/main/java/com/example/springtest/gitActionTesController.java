5f820dd5cbe61e662cea69b8301d376eda03f2c6
faulty337 <faulty337@gmail.com>
2022-12-28 10:32:48 +0900

    Refactor : 디렉토리 재구성

---
+++ src/main/java/com/example/springtest/gitActionTesController.java
@@ -0,0 +1,22 @@
+package com.example.springtest;
+
+import com.example.springtest.common.ResponseMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class gitActionTesController {
+
+    @Value("${secret.key}")
+    private String key;
+
+
+    @GetMapping("/secret")
+    public ResponseEntity<ResponseMessage> getSecretKey(){
+        ResponseMessage<String> responseMessage = new ResponseMessage<>("Success", 200, key);
+        return new ResponseEntity<>(responseMessage, HttpStatus.valueOf(responseMessage.getStatusCode()));
+    }
+}
---
+++ src/main/java/com/example/springtest/mvcTest/service/TestService.java
@@ -0,0 +1,10 @@
+package com.example.springtest.mvcTest.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+
+}
